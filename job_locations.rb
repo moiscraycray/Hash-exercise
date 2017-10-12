a2d5b7524d235b8ed0d13b138616a4748104167d
Olivia Mo <oliviatrademark@gmail.com>
2017-10-12 14:33:25 +1100

    first commit

---
+++ job_locations.rb
@@ -0,0 +1,66 @@
+# E1. Given the following hash, write a program that outputs the jobs located in Ultimo.
+#
+job_locations = {
+    ultimo: ["Front End", "Dev Ops", "Full stack"],
+    surry_hills: ["Desginer", "Back End"],
+    brisbane: ["Full stack", "Front End"],
+    melbourne: ["Front End","Full stack","Back End"]
+}
+
+job_locations.each do |k, v|
+  if k == :ultimo
+    p v
+  end
+end
+
+puts
+puts "#" * 50
+puts
+
+# E2. Write a program the outputs each of the `job_locations` in key-value arrangement.
+# Use the `each_key` and `each_value` to iterate through the keys and values separately.
+# Then, output both in "#{key} => #{value}" format.
+
+job_locations.each do |k, v|
+  puts "#{k}: #{v}"
+end
+
+puts
+puts "#" * 50
+puts
+
+# E3. Write a program that uses the `select` method on the `job_locations` hash to output only the jobs in the Sydney area.
+values = Array.new
+sydney = job_locations.select do |k, v|
+  if k == :ultimo || k == :surry_hills then values << v end
+end
+  p values.flatten
+
+puts
+puts "#" * 50
+puts
+
+# E4. Write a program that illustrates the difference between `merge` and `merge!` on ruby hashes.
+#
+# eg. sample data (feel free to use your own hashes instead)
+
+# options = {width: "10em"}
+# extra_options = {height: "11em"}
+#
+# Example output:
+# options.merge(extra_options): {:width=>"10em", :height=>"11em"}
+# options: {:width=>"10em"}
+# extra_options: {:height=>"11em"}
+# options after merge!(extra_options) :
+# {:width=>"10em", :height=>"11em"}
+
+p fruit = {banana: 5, strawberry: 9, orange: 23, blueberry: 3}
+p meat = {chicken: 5, beef: 21, pork: 43, lamb: 31}
+
+puts "\nmerge"
+fruit.merge(meat)
+p fruit
+
+puts "\nmerge!"
+fruit.merge!(meat)
+p fruit
